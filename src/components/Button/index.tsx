91275d4fb54ee15946957b6c1eaf2c78dda41ba5
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2022-06-29 14:04:10 -0300

    corrigindo env

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Button, View, Text} from 'react-native';
 import {REACT_APP_API} from '@env';
+import {api} from '../../services/api';
 
 const Login: React.FC = () => {
+    const HandleTest = async () => {
+        const response = await api.get('/');
+        console.log(response.data);
+    };
     return (
         <View>
-            <Button title="test" />
+            <Button title="test" onPress={HandleTest} />
             <Text>{REACT_APP_API}</Text>
         </View>
     );
